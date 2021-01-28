50b0bd2354a87100755195bf4b1ddc5827bd1851
Johan Hägnander <johan.hagnander@movinity.ai>
2021-01-28 14:04:27 +0100

    feat: possible to play again after game over

--- src/main.cpp
+++ src/main.cpp
@@ -22,27 +22,35 @@ int main() {
   std::string name = "nobody";
   std::cin >> name;
   player.Name(name);
-  Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
-  Controller controller;
-  Game game(kGridWidth, kGridHeight);
-  game.Run(controller, renderer, kMsPerFrame);
-  std::cout << "Game has terminated successfully!\n";
-  //Update High Score list and return rank (save 100 values in high score list)
-  int Rank = fileparser.UpdateHighScoreList(player.Name(),game.GetScore());
-  //Write to file
-  fileparser.WriteFile();
-  int PersonalBestRank = fileparser.GetPersonalBestRank(player.Name());
-  //Inform the player about best postion in high score list
-  if (Rank == PersonalBestRank)
-    std::cout << "Congrats " << player.Name() << "! you set a new personal record and reached pos = " << Rank << " on High-score Table \n";
-  else if (Rank <= kMaxEntries)
-    std::cout << "Congrats " << player.Name() << "! you reached pos = " << Rank << " on High-score Table \n";
-  else
-    std::cout << "You did not reach the High-score Table \n";
+  bool ContinuePlaying = true;
+  while (ContinuePlaying == true)
+  {
+    Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
+    Controller controller;
+    Game game(kGridWidth, kGridHeight);
+    game.Run(controller, renderer, kMsPerFrame);
+    std::cout << "Game has terminated successfully!\n";
+    //Update High Score list and return rank (save 100 values in high score list)
+    int Rank = fileparser.UpdateHighScoreList(player.Name(),game.GetScore());
+    //Write to file
+    fileparser.WriteFile();
+    int PersonalBestRank = fileparser.GetPersonalBestRank(player.Name());
+    //Inform the player about best postion in high score list
+    if (Rank == PersonalBestRank)
+      std::cout << "Congrats " << player.Name() << "! you set a new personal record and reached pos = " << Rank << " on High-score Table \n";
+    else if (Rank <= kMaxEntries)
+      std::cout << "Congrats " << player.Name() << "! you reached pos = " << Rank << " on High-score Table \n";
+    else
+      std::cout << "You did not reach the High-score Table \n";
   
-  std::cout << "Score: " << game.GetScore() << "\n";
-  std::cout << "Size: " << game.GetSize() << "\n";
-  fileparser.PrintTop5();
-
+    std::cout << "Score: " << game.GetScore() << "\n";
+    std::cout << "Size: " << game.GetSize() << "\n";
+    fileparser.PrintTop5();
+    std::string PlayAgain;
+    std::cout << "If you want to play again type y and hit enter \n";
+    std::cin >> PlayAgain;
+    if (PlayAgain != "y")
+      ContinuePlaying = false;
+  }
   return 0;
 }
